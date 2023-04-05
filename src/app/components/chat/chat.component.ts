95a94b0f7993a1209daf870f8c69b2f0849d78bb
Eito Takase <eito01178@gmail.com>
2023-04-05 21:25:53 +0900

    メッセージの初期化タイミングを変える

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -18,13 +18,7 @@ export class ChatComponent {
   /** APIにポストするデータ */
   private data: GptPostData = {
     model: 'gpt-3.5-turbo',
-    messages: [
-      {
-        role: 'system',
-        content:
-          'あなたは優秀なアシスタントです、以下の文章の内容をよく読んで、要点を要約してください。また日本語で回答してください。ユーザーからの質問に回答する際は、文章内にある情報が有用な場合、文章内の情報を優先して利用してください',
-      },
-    ],
+    messages: [],
   };
 
   isFetching = false;
@@ -38,10 +32,17 @@ export class ChatComponent {
     this.clientContentService.getArticle$().subscribe(
       (article) => {
         if (article && article.content) {
-          this.data.messages.push({
-            role: 'system',
-            content: article.content,
-          });
+          this.data.messages = [
+            {
+              role: 'system',
+              content:
+                'あなたは優秀なアシスタントです、以下の文章の内容をよく読んで、要点を要約してください。また日本語で回答してください。ユーザーからの質問に回答する際は、文章内にある情報が有用な場合、文章内の情報を優先して利用してください',
+            },
+            {
+              role: 'system',
+              content: article.content,
+            },
+          ];
           this.fetchCompletions(this.data);
         }
       },
