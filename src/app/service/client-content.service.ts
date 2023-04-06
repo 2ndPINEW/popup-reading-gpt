c7b0b0b4773ef6c00499728b125527a11d3ceeb1
Eito Takase <eito01178@gmail.com>
2023-04-06 13:00:05 +0900

    ♻️ネスト酷かったからリファクタ

--- src/app/service/client-content.service.ts
+++ src/app/service/client-content.service.ts
@@ -1,57 +1,64 @@
 import { Injectable } from '@angular/core';
-import { from, Observable, of } from 'rxjs';
+import { from, map, Observable, of, switchMap } from 'rxjs';
 import { ArticleData, extractFromHtml } from '@extractus/article-extractor';
 import { environment } from 'src/environments/environment';
 
 export declare const chrome: any;
 
-/** クライアントページに表示されているコンテンツを取得するサービス */
 @Injectable({
   providedIn: 'root',
 })
 export class ClientContentService {
-  /**
-   * ページに表示されている記事コンテンツを取得する関数
-   */
+  // ページに表示されている記事コンテンツを取得する関数
   getArticle$(): Observable<ArticleData | null> {
-    // 開発環境ではサンプルデータを返す
     if (!environment.production) {
+      // 開発環境ではサンプルデータを返す
       return of({ content: 'This is sample content' });
     }
-    // 記事データを返すObservable
-    return from(
-      new Promise<ArticleData | null>(async (res) => {
-        // 現在開いているタブの情報を取得
-        const tabs = await chrome.tabs.query({
-          active: true,
-          currentWindow: true,
-        });
-        const { id, url } = tabs[0];
 
-        // 開いているタブでHTMLを返す関数を実行する
-        const injectionResults = await chrome.scripting.executeScript({
-          target: { tabId: id },
-          func: () => {
-            {
-              const html = document.querySelector('html')?.outerHTML;
-              return html;
-            }
-          },
-        });
-        // 実行結果からHTMLを取得する
-        const html = injectionResults[0].result;
-        // HTMLから記事データを取得する
-        const article = await extractFromHtml(html, url);
-        // 記事データの本文に含まれるHTMLタグを除去する
-        if (article?.content) {
-          article.content = article?.content?.replace(
-            /(<[^>]+>|\{[^}]+\})/g,
-            ''
-          );
-        }
-        // 記事データを返す
-        res(article);
-      })
+    // 開いているタブの記事データを返すObservable
+    return from(this.getTabInfo()).pipe(
+      switchMap(({ tabId, url }) =>
+        this.getTabHtml$(tabId).pipe(
+          switchMap((html) => this.extractArticleData(html, url))
+        )
+      )
     );
+  }
+
+  // 現在開いているタブの情報を取得する関数
+  private async getTabInfo(): Promise<{ tabId: number; url: string }> {
+    const tabs = await chrome.tabs.query({
+      active: true,
+      currentWindow: true,
+    });
+    return { tabId: tabs[0].id, url: tabs[0].url };
+  }
+
+  // 指定したタブのHTMLを取得する関数
+  private getTabHtml$(tabId: number): Observable<string> {
+    return from(
+      chrome.scripting.executeScript({
+        target: { tabId },
+        func: () => {
+          {
+            const html = document.querySelector('html')?.outerHTML;
+            return html;
+          }
+        },
+      })
+    ).pipe(map((injectionResults: any) => injectionResults[0].result));
+  }
+
+  // HTMLから記事データを取得する
+  private async extractArticleData(
+    html: string,
+    url: string
+  ): Promise<ArticleData | null> {
+    const article = await extractFromHtml(html, url);
+    if (article?.content) {
+      article.content = article?.content?.replace(/(<[^>]+>|\{[^}]+\})/g, '');
+    }
+    return article;
   }
 }
